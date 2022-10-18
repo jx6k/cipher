4a88a71754f95b0634b24e7d6cde2b82b1829c23
jx6k <111664257+jx6k@users.noreply.github.com>
2022-10-18 13:46:53 +0100

    added tools
    caesar cipher

--- script/caesar.js
+++ script/caesar.js
@@ -4,39 +4,25 @@ const output = document.querySelector("textarea.output")
 const encrypter = document.querySelector("button.encrypt")
 const decrypter = document.querySelector("button.decrypt")
 
-const gradient = document.querySelector("input.gradient")
-const intercept = document.querySelector("input.intercept")
+const shift = document.querySelector("input.shift")
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
 
 const mod = (a, b) => {
     return ((a % b) + b) % b
 }
 
-const multiplicativeInverses = [
-    [1, 1],
-    [3, 9],
-    [5, 21],
-    [7, 15],
-    [9, 3],
-    [11, 19],
-    [15, 7],
-    [17, 23],
-    [19, 11],
-    [21, 5],
-    [23, 17],
-    [25, 25]
-]
-
-const alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
-
-const encrypt = (initial, a, b) => {
+const encrypt = (initial, a) => {
     const splitnitial = initial.toLowerCase().split("")
     let final = ""
 
     for (let i = 0; i < splitnitial.length; i++) {
         const position = alphabet.findIndex(o => o == splitnitial[i])
 
+        console.log(position + a)
+
         if (position != -1) {
-            final += alphabet[(position * a + b) % alphabet.length]
+            final += alphabet[mod(position + a, alphabet.length)]
         }
         else {
             final += splitnitial[i]
@@ -46,20 +32,15 @@ const encrypt = (initial, a, b) => {
     return final
 }
 
-const decrypt = (initial, a, b) => {
+const decrypt = (initial, a) => {
     const splitnitial = initial.toLowerCase().split("")
     let final = ""
-
-    a = mod(a, alphabet.length)
-    b = mod(b, alphabet.length)
-
-    const inverse = multiplicativeInverses[multiplicativeInverses.findIndex(x => x[0] == a)][1]
 
     for (let i = 0; i < splitnitial.length; i++) {
         const position = alphabet.findIndex(o => o == splitnitial[i])
 
         if (position != -1) {
-            final += alphabet[mod((position - b) * inverse, alphabet.length)]
+            final += alphabet[mod(position - a, alphabet.length)]
         }
         else {
             final += splitnitial[i]
@@ -70,17 +51,8 @@ const decrypt = (initial, a, b) => {
 }
 
 encrypter.onclick = () => {
-    output.value = encrypt(
-        input.value,
-        parseInt(gradient.value),
-        parseInt(intercept.value)
-    )
+    output.value = encrypt(input.value, parseInt(shift.value))
 }
-
 decrypter.onclick = () => {
-    output.value = decrypt(
-        input.value,
-        parseInt(gradient.value),
-        parseInt(intercept.value)
-    )
+    output.value = decrypt(input.value, parseInt(shift.value))
 }
